ac179b0131b03b3c96411b0da0b79c266d1b38c9
biplab-jha <152853196+biplab-jha@users.noreply.github.com>
2025-07-18 18:54:43 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Hero from '@components/custom/Hero';
+
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
